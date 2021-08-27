55779b85e0196d05816eb2ab01c688476cf532a9
David Uebelacker <david.endress@endress.com>
2021-08-27 10:50:57 +0200

    :ok_hand: added ActionController::Live to enable real streaming

--- app/controllers/firmwares_controller.rb
+++ app/controllers/firmwares_controller.rb
@@ -1,4 +1,6 @@
 class FirmwaresController < ApplicationController
+  include ActionController::Live
+
   def index
     @firmwares = Firmware.all
   end
@@ -26,6 +28,7 @@ class FirmwaresController < ApplicationController
     @firmware = Firmware.find(params[:firmware_id])
     response.headers['Content-Type'] = @firmware.file.content_type
     response.headers['Content-Disposition'] = ContentDisposition.format(disposition: 'attachment', filename: @firmware.file.filename.to_s)
+    response.headers['X-Accel-Buffering'] = 'no'
     @firmware.file.download do |chunk|
       response.stream.write(chunk)
     end
